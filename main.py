d72c84b8f9f4c6bfd9c848203c1d2fdd25495f44
Nandini Pandey <n.pandey1222@gmail.com>
2026-05-09 17:58:45 +0530

    Added session saving to file

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 subject = input("What did you study? ")
 minutes = input("How many minutes? ")
 
-print(f"You studied {subject} for {minutes} minutes.")
+session = f"{subject} - {minutes} mins\n"
+
+with open("sessions.txt", "a") as file:
+    file.write(session)
+
+print("Session saved.")
