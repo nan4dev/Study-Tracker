1d1f717050771d53a5458c43b7f136f34880371f
Nandini Pandey <n.pandey1222@gmail.com>
2026-05-09 18:39:35 +0530

    Added total study time calculation

--- main.py
+++ main.py
@@ -6,4 +6,13 @@ session = f"{subject} - {minutes} mins\n"
 with open("sessions.txt", "a") as file:
     file.write(session)
 
-print("Session saved.")
+total = 0
+
+with open("sessions.txt", "r") as file:
+    for line in file:
+        parts = line.split(" - ")
+        time_part = parts[1]
+        mins = int(time_part.replace(" mins\n", ""))
+        total += mins
+
+print(f"Total study time: {total} minutes")
