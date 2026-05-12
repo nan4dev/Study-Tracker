d725b9eb0479e5334613b82464bc85ce35baaccd
Nandini Pandey <n.pandey1222@gmail.com>
2026-05-12 17:07:44 +0530

    Added study history display

--- main.py
+++ main.py
@@ -6,13 +6,18 @@ session = f"{subject} - {minutes} mins\n"
 with open("sessions.txt", "a") as file:
     file.write(session)
 
+print("\nStudy History:")
+
 total = 0
 
 with open("sessions.txt", "r") as file:
     for line in file:
+        print(line.strip())
+
         parts = line.split(" - ")
         time_part = parts[1]
+
         mins = int(time_part.replace(" mins\n", ""))
         total += mins
 
-print(f"Total study time: {total} minutes")
+print(f"\nTotal study time: {total} minutes")
